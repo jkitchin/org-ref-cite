7ae0bd2d1341926a2366c265cf575168ec09f90b
John Kitchin <jkitchin@andrew.cmu.edu>
2021-07-30 08:26:54 -0400

    another probable fix for issue #9
    make sure we get a string even when it is empty.

--- org-ref-cite-core.el
+++ org-ref-cite-core.el
@@ -109,7 +109,7 @@ This annotator just looks up the cite LaTeX command for the style."
   (let* ((w (+  (- 5 (length s)) 20)))
     (concat (make-string w ? )
 	    (propertize
-	     (cdr (assoc s org-ref-cite-styles))
+	     (or (cdr (assoc s org-ref-cite-styles)) "")
 	     'face 'org-ref-cite-annotate-style-face))))
 
 
@@ -179,7 +179,6 @@ If point is on a citation, it makes an export preview of the citation with the s
 				  (concat "/" style))
 		  (org-element-interpret-data refs)))
     (goto-char cp)))
-
 
 
 ;; * Navigation functions
